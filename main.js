20a02c7493ca9ef9f968ccf64da181e99303eb02
vuthienan178 <vuthienan178@gmail.com>
2017-11-04 13:47:13 +0700

    Add files via upload
    dfgfdgfdg

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ function playStream(idVideoTag, stream) {
 
 const peer = new Peer({ 
     key: 'peerjs', 
-    host: 'mypeer3005.herokuapp.com', 
+    host: 'https://rtcstream.herokuapp.com/', 
     secure: true, 
     port: 443, 
     config: customConfig 
