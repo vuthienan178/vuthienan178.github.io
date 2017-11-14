f840442312d30c59a785a76d2f85a1d95aad5f13
vuthienan178 <vuthienan178@gmail.com>
2017-11-14 23:00:19 +0700

    Update main.js
    change secret key

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $.ajax({
   url: "https://service.xirsys.com/ice",
   data: {
     ident: "vuthienan178",
-    secret: "7ed096e8-bfd3-11e7-981e-3a716423545a",
+    secret: "d2118170-c954-11e7-a033-90d088cfa6d3",
     domain: "vuthienan178.github.io",
     application: "default",
     room: "default",
