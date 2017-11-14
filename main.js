3d2738bc630893bf2c659d42fe2701d7b199a20f
vuthienan178 <vuthienan178@gmail.com>
2017-11-14 20:19:18 +0700

    Add files via upload
    asdfdsf

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const socket = io('https://streaminganvu.herokuapp.com');
+  // socket online
+//const socket = io('https://streaminganvu.herokuapp.com');
+//socket local
+const socket = io('http://localhost:3000');
+
+let customconfig;
 
 $('#div-stream').hide();
+
+$.ajax({
+    url:"",
+    data:{
+        ident:"vuthienan178",
+        secret:"7ed096e8-bfd3-11e7-981e-3a716423545a",
+        domain:"vuthienan178.github.io",
+        application:"default",
+        room:"default",
+        secure:1
+    },
+    success: function(data,status){
+            customconfig = data.d;
+            console.log(customconfig);
+    },
+    async: false
+})
+
 
 socket.on('ONLINE_USER_ARRAY',arrUser =>{
         $('#div-stream').show();
@@ -28,7 +51,7 @@ socket.on('SIGNUP_FAILED', () => alert('VUI LONG CHON USERNAME KHAC'));
 
 
 function openStream(){
-    const config = {audio : true, video : true};
+    const config = {audio : false, video : true};
     return navigator.mediaDevices.getUserMedia(config);
 };
 
@@ -45,7 +68,8 @@ const peer = new Peer({
     key: 'peerjs',
     host:'rtcstream.herokuapp.com',
      secure : true,
-    port :443
+    port :443,
+    config:customconfig
 });
 peer.on('open',id => {
     $('#my-peer').append(id);
