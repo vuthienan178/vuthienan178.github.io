dc36f0c42786de83748b9c6b26037f98ba499975
vuthienan178 <vuthienan178@gmail.com>
2017-11-14 20:40:59 +0700

    Update main.js
    commit audio

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ socket.on('SIGNUP_FAILED', () => alert('VUI LONG CHON USERNAME KHAC'));
 
 
 function openStream(){
-    const config = {audio : false, video : true};
+    const config = {audio : true, video : true};
     return navigator.mediaDevices.getUserMedia(config);
 };
 
